843cac0695d70fc12a965dcb7932d2a1773e92e3
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2021-11-11 18:07:11 -0600

    fixed time formatting evalutaion

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 //Global Variables
 
 var currentD = moment().format("dddd, MMMM Do YYYY");
-var currentH = moment().format("hh");
+var currentH = moment().format("k");
 var workHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
 var calendarNotes = ["", "", "", "", "", "", "", "", ""];
 
@@ -89,6 +89,8 @@ function buildDay () {
 function getTimeClass(evaluateMe) {
 
     console.log("getTimeClass is running");
+    console.log("evaluateMe is " + evaluateMe);
+    console.log("currentH is " +currentH);
 
     if (evaluateMe < currentH) {
 
